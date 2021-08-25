270298bdd9671acf203bb553df1ca6542430330e
hyerim <yhrim0729@naver.com>
2021-08-25 21:57:13 +0900

    myBatis오류

--- EateryZip/src/main/java/com/sixnicorn/eateryzip/user/dao/BStoreDao.java
+++ EateryZip/src/main/java/com/sixnicorn/eateryzip/user/dao/BStoreDao.java
@@ -1,5 +1,13 @@
 package com.sixnicorn.eateryzip.user.dao;
 
+import com.sixnicorn.eateryzip.user.dto.BStoreDto;
+
 public interface BStoreDao {
 
+	// 가게정보 등록하기
+	public void insert(BStoreDto dto);
+	// 가게정보 보기
+	public BStoreDto StoreView(String b_id);
+	// 가게정보 수정하기
+	public void update(BStoreDto dto);
 }
