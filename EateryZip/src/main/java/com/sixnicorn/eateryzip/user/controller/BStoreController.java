399e129109ca46f2610310ed4a313b6ab64ffb07
hyerim <yhrim0729@naver.com>
2021-08-26 04:53:35 +0900

    가게등록 페이지 (사업자번호 불러오고)

--- EateryZip/src/main/java/com/sixnicorn/eateryzip/user/controller/BStoreController.java
+++ EateryZip/src/main/java/com/sixnicorn/eateryzip/user/controller/BStoreController.java
@@ -4,9 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.sixnicorn.eateryzip.user.dto.BStoreDto;
 
 @Controller
 public class BStoreController {
